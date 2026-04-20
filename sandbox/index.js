243ce629dc88037b15d547f31f1a241bce117def
WilliamBagel <102871850+WilliamBagel@users.noreply.github.com>
2026-04-20 18:04:02 -0400

    Fix Save-Load Init 3-Debug

--- sandbox/index.js
+++ sandbox/index.js
@@ -52,6 +52,7 @@ const srcdoc = `
 
                 let initialized = false;
                 window.addEventListener('message', (e)=>{
+                    console.log(e,"mid2");
                     if(e?.data?.type === 'load'){
                         sandbox.contentWindow.postMessage({type:'load', content: e.data.content}, '*');
                     } else if(e?.data?.type === 'save' && e?.origin === 'null'){
@@ -67,6 +68,7 @@ const srcdoc = `
 sandbox.srcdoc = srcdoc;
 
 window.addEventListener('message', (e) => {
+    console.log(e,"mid1");
     if (e?.data?.type === 'load' && parentOrigins.indexOf(e?.origin) !== -1) {
         sandbox.contentWindow.postMessage({ type: 'load', content: e.data.content }, '*');
     } else if (e?.data?.type === 'save' && e?.origin === 'null') {
